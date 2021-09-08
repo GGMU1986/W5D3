8b41c0ac450869d147d96d5628abe8ada2ec30c5
gtsimis <gt2186@gmail.com>
2021-09-08 15:15:12 -0400

    Added users class

--- users.rb
+++ users.rb
@@ -2,7 +2,14 @@ require_relative "quesiondatabase"
 
 class User
 
-    def initialize
-        
+    def self.find_by_name(fname, lname)
+        u = QuestionsDataBase.instance.execute(SELECT * FROM users WHERE users.fname = fname AND users.lname = lname
+        User.new(u)
+    end
+
+    def initialize(options)
+       @id = options['id']
+       @fname = options['fname'] 
+       @lname = options['lname'] 
     end
 end
