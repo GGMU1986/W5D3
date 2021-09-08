01b05fbb84f8288f8abbc0a8b90849511bcd62ed
spark <spark11491@gmail.com>
2021-09-08 14:55:27 -0400

    finish initialize questions

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
@@ -15,7 +21,7 @@ CREATE TABLE questions(
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE question_follows 
+CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
@@ -42,7 +48,7 @@ CREATE TABLE question_likes (
     question_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (question_id) REFERENCES question(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 INSERT INTO
@@ -57,15 +63,20 @@ INSERT INTO
 VALUES
     ('Got Snacks?', 'What are the snacks that we are going to eat tonight?', (SELECT id FROM users WHERE lname = 'Tsimis')),
     ('Where are we going?', 'Where are we going this weekend?', (SELECT id FROM users WHERE lname = 'Park')),
-    ('Kryptonite?', 'Is there any Kryptonite in this auditorium?', (SELECT id from users WHERE lanme = 'Man'));
+    ('Kryptonite?', 'Is there any Kryptonite in this auditorium?', (SELECT id from users WHERE lname = 'Man'));
 
 INSERT INTO
     replies(question_id, parent_id, user_id, body)
 VALUES
     ((SELECT id FROM questions WHERE title = 'Got Snacks?'), NULL, (SELECT id FROM users WHERE lname = 'Park'), 'Snacks for tonight are pizza, hot wings, and popcorn'),
     ((SELECT id FROM questions WHERE title = 'Where are we going?'), NULL,(SELECT id FROM users WHERE lname = 'Man'), 'We are going hiking'),
-    ((SELECT id FROM questions WHERE title = 'Kryptonite?'), NULL, (SELECT id FROM users WHERE lname = 'Tsimis'), 'Yes, there is'),
-    ((SELECT id FROM questions WHERE title = 'Got Snacks?'), (SELECT id FROM replies WHERE body = 'Snacks for tonight are pizza, hot wings, and popcorn' ),(SELECT id FROM users WHERE lname = 'Man'), 'That is a whole meal');
+    ((SELECT id FROM questions WHERE title = 'Kryptonite?'), NULL, (SELECT id FROM users WHERE lname = 'Tsimis'), 'Yes, there is');
+
+INSERT INTO
+    replies(question_id, parent_id, user_id, body)
+VALUES
+    ((SELECT id FROM questions WHERE title = 'Got Snacks?'), (SELECT id FROM replies WHERE body = 'Snacks for tonight are pizza, hot wings, and popcorn'), (SELECT id FROM users WHERE lname = 'Man'), 'That is a whole meal'),
+    ((SELECT id FROM questions WHERE title = 'Kryptonite?'), (SELECT id FROM replies WHERE body = 'Yes, there is'), (SELECT id FROM users WHERE lname = 'Man'), 'How do you know?');
 
 INSERT INTO 
     question_follows(user_id, question_id)
